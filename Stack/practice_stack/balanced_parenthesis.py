9cc9d2b1d4a70b8af1b40ecb6671e4fbc8e418dd
pu4j4 <bhanubethala2@gmail.com>
2026-01-08 23:11:54 +0530

    #

---
+++ Stack/practice_stack/balanced_parenthesis.py
@@ -0,0 +1,14 @@
+def is_balanced(s):
+    stack = []
+    mapping = {')':'(','}':'{',']':'['}
+    for ch in s:
+        if ch in '({[':
+            stack.append(ch)
+        elif ch in ')}]':
+            if not stack or stack[-1] != mapping[ch]:
+                return False
+            stack.pop()
+    return len(stack) == 0
+print(is_balanced('[]{}()'))
+print(is_balanced('(]{})'))
+print(is_balanced('([{}])'))
