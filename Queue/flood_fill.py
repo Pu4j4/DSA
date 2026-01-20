0c0bf39f8cdd1328322dd80e80af7b8c449b4310
pu4j4 <bhanubethala2@gmail.com>
2026-01-20 11:40:00 +0530

    #

--- Queue/flood_fill.py
+++ Queue/flood_fill.py
@@ -21,3 +21,26 @@
     #
     # return image
 
+#optmized
+def flood_fill(image, sr, sc, color):
+    rows, cols = len(image), len(image[0])
+    original = image[sr][sc]
+
+    if original == color:
+        return image
+
+    def dfs(r, c):
+        if r < 0 or r >= rows or c < 0 or c >= cols:
+            return
+        if image[r][c] != original:
+            return
+
+        image[r][c] = color
+
+        dfs(r + 1, c)
+        dfs(r - 1, c)
+        dfs(r, c + 1)
+        dfs(r, c - 1)
+
+    dfs(sr, sc)
+    return image
