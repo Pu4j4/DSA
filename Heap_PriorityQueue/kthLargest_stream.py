b2294bb648130cc3fbb4e59341709f207bf9c2d4
pu4j4 <bhanubethala2@gmail.com>
2026-01-21 13:58:01 +0530

    #

---
+++ Heap_PriorityQueue/kthLargest_stream.py
@@ -0,0 +1,14 @@
+#brute force
+# def __init__(self, k, nums):
+#     self.k = k
+#     self.nums = nums
+#
+#
+# def add(self, val):
+#     self.nums.append(val)
+#     self.nums.sort(reverse=True)
+#     return self.nums[self.k - 1]
+
+
+
+
