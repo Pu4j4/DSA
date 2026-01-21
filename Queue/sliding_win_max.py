0be4251419584a3d894afe3b05b724aae11338c2
pu4j4 <bhanubethala2@gmail.com>
2026-01-21 09:24:34 +0530

    #

---
+++ Queue/sliding_win_max.py
@@ -0,0 +1,17 @@
+#brute force
+# def max_sliding_window(nums, k):
+#     n = len(nums)
+#     result = []
+#     for i in range(n - k + 1):
+#         max_val = nums[i]
+#         for j in range(i, i + k):
+#             max_val = max(max_val, nums[j])
+#         result.append(max_val)
+#     return result
+
+
+
+
+
+
+
