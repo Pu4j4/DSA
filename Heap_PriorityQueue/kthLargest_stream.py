633edc5d12ff263e1d6539075f9d3bb22430f92b
pu4j4 <bhanubethala2@gmail.com>
2026-01-22 10:40:27 +0530

    #

--- Heap_PriorityQueue/kthLargest_stream.py
+++ Heap_PriorityQueue/kthLargest_stream.py
@@ -10,5 +10,23 @@
 #     return self.nums[self.k - 1]
 
 
+#optimized
+import heapq
+def __init__(self, k, nums):
+    self.k = k
+    self.heap = nums
+    heapq.heapify(self.heap)
+
+    while len(self.heap) > k:
+        heapq.heappop(self.heap)
+
+
+def add(self, val: int) -> int:
+    heapq.heappush(self.heap, val)
+
+    if len(self.heap) > self.k:
+        heapq.heappop(self.heap)
+
+    return self.heap[0]
 
 
