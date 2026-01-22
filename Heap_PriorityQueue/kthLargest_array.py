4f42020978828262b69b49346a4416fe0713bc21
pu4j4 <bhanubethala2@gmail.com>
2026-01-22 10:41:12 +0530

    #

--- Heap_PriorityQueue/kthLargest_array.py
+++ Heap_PriorityQueue/kthLargest_array.py
@@ -1,4 +1,16 @@
 #Brute force
+# def find_kth_largest(nums, k):
+#     nums.sort(reverse=True)
+#     return nums[k - 1]
+
+#Optimized
+import heapq
 def find_kth_largest(nums, k):
-    nums.sort(reverse=True)
-    return nums[k - 1]
+    heap = []
+
+    for num in nums:
+        heapq.heappush(heap, num)
+        if len(heap) > k:
+            heapq.heappop(heap)
+
+    return heap[0]
