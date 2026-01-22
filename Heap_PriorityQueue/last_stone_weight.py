6cac47e82dcce51ae0456f5a92b263972752f5dc
pu4j4 <bhanubethala2@gmail.com>
2026-01-22 10:40:02 +0530

    #

--- Heap_PriorityQueue/last_stone_weight.py
+++ Heap_PriorityQueue/last_stone_weight.py
@@ -1,9 +1,27 @@
 #brute force
+# def last_stone_weight(stones):
+#     while len(stones) > 1:
+#         stones.sort()
+#         y = stones.pop()
+#         x = stones.pop()
+#         if y != x:
+#             stones.append(y-x)
+#     return stones[0]  if stones else 0
+
+
+#optimized
+import heapq
 def last_stone_weight(stones):
-    while len(stones) > 1:
-        stones.sort()
-        y = stones.pop()
-        x = stones.pop()
+    heap = [-s for s in stones]
+    heapq.heapify(heap)
+
+    while len(heap) > 1:
+        y = -heapq.heappop(heap)
+        x = -heapq.heappop(heap)
+
         if y != x:
-            stones.append(y-x)
-    return stones[0]  if stones else 0
+            heapq.heappush(heap, -(y - x))
+
+    return -heap[0] if heap else 0
+
+
