1c90b06adb682e0988afad4a0df0b713316ecd06
pu4j4 <bhanubethala2@gmail.com>
2026-01-24 12:57:44 +0530

    #

--- Heap_PriorityQueue/task_scheduler.py
+++ Heap_PriorityQueue/task_scheduler.py
@@ -7,5 +7,28 @@ def least_interval(tasks, n):
 
     return max(len(tasks), (max_freq - 1) * (n + 1) + count_max)
 
+#optimized
+def least_interval(tasks, n):
+    freq = Counter(tasks)
+    cooldown = {}
+    time = 0
+
+    while freq:
+        available = []
+        for task in freq:
+            if task not in cooldown or cooldown[task] <= time:
+                available.append(task)
+
+        if available:
+            task = max(available, key=lambda x: freq[x])
+            freq[task] -= 1
+            if freq[task] == 0:
+                del freq[task]
+            cooldown[task] = time + n + 1
+
+        time += 1
+
+    return time
+
 
 
