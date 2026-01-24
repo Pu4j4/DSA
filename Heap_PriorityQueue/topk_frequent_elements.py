0ad08fb320beefb3285a765850e7693beb3a9918
pu4j4 <bhanubethala2@gmail.com>
2026-01-24 12:57:15 +0530

    #

--- Heap_PriorityQueue/topk_frequent_elements.py
+++ Heap_PriorityQueue/topk_frequent_elements.py
@@ -9,3 +9,17 @@ def top_k_frequent(nums, k):
 
     return [arr[i][0] for i in range(k)]
 
+#optmized
+import heapq
+def top_k_frequent(nums, k):
+    freq = {}
+    for num in nums:
+        freq[num] = freq.get(num, 0) + 1
+
+    heap = []
+    for num, count in freq.items():
+        heapq.heappush(heap, (count, num))
+        if len(heap) > k:
+            heapq.heappop(heap)
+
+    return [num for count, num in heap]
