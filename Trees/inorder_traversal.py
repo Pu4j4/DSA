8c90bb7e793a5f76ca1b63f3dc2ee19e57418f12
pu4j4 <bhanubethala2@gmail.com>
2026-01-27 08:54:01 +0530

    #

---
+++ Trees/inorder_traversal.py
@@ -0,0 +1,16 @@
+#brute force
+def inorder_traversal(self, root):
+    result = []
+    stack = []
+    current = root
+
+    while current or stack:
+        while current:
+            stack.append(current)
+            current = current.left
+
+        current = stack.pop()
+        result.append(current.val)
+        current = current.right
+
+    return result
