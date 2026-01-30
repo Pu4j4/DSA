db3013495ad4ac44bc10edf256b102bab4fc5638
pu4j4 <bhanubethala2@gmail.com>
2026-01-30 22:05:33 +0530

    #

--- Backtracking/subsets_ii.py
+++ Backtracking/subsets_ii.py
@@ -13,3 +13,23 @@ def subsetsWithDup(self, nums):
     nums.sort()
     backtrack(0, [])
     return list(result)
+
+
+#Optimized
+def subsetsWithDup(self, nums):
+    nums.sort()
+    result = []
+
+    def backtrack(start, path):
+        result.append(path[:])
+
+        for i in range(start, len(nums)):
+            if i > start and nums[i] == nums[i - 1]:
+                continue
+
+            path.append(nums[i])
+            backtrack(i + 1, path)
+            path.pop()
+
+    backtrack(0, [])
+    return result
