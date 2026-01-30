402dc965d0e6c4bb7367ac396603414e80004595
pu4j4 <bhanubethala2@gmail.com>
2026-01-30 22:02:46 +0530

    #

--- Backtracking/subset.py
+++ Backtracking/subset.py
@@ -9,3 +9,21 @@ def subsets(self, nums):
         result.extend(new_subsets)
 
     return result
+
+
+#Optimized
+def subsets(self, nums):
+    result = []
+
+    def backtrack(start, path):
+        result.append(path[:])
+
+        for i in range(start, len(nums)):
+            path.append(nums[i])
+            backtrack(i + 1, path)
+            path.pop()
+
+    backtrack(0, [])
+    return result
+
+
