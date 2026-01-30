56321566659d2076b3b050d3b8ac0fcac1f61af7
pu4j4 <bhanubethala2@gmail.com>
2026-01-30 22:01:43 +0530

    #

---
+++ Backtracking/subset.py
@@ -0,0 +1,11 @@
+#brute force
+def subsets(self, nums):
+    result = [[]]
+
+    for num in nums:
+        new_subsets = []
+        for subset in result:
+            new_subsets.append(subset + [num])
+        result.extend(new_subsets)
+
+    return result
