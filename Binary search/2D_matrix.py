7db8f9785e57a00bcce16741dc83337bc72840b3
pu4j4 <bhanubethala2@gmail.com>
2026-01-31 16:34:41 +0530

    #

--- Binary search/2D_matrix.py
+++ Binary search/2D_matrix.py
@@ -6,3 +6,22 @@ def searchMatrix(self, matrix, target):
                 return True
     return False
 
+
+#optimized
+def searchMatrix(self, matrix, target):
+    rows, cols = len(matrix), len(matrix[0])
+    left, right = 0, rows * cols - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+        r = mid // cols
+        c = mid % cols
+
+        if matrix[r][c] == target:
+            return True
+        elif matrix[r][c] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return False
