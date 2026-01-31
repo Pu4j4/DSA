f590fcdcbfa60217ee4b6a784f0f153e90567d64
pu4j4 <bhanubethala2@gmail.com>
2026-01-31 16:37:22 +0530

    #

--- Binary search/binary_search.py
+++ Binary search/binary_search.py
@@ -5,4 +5,20 @@ def search(self, nums, target):
             return i
     return -1
 
+#optimized
+def search(self, nums: List[int], target: int) -> int:
+    left, right = 0, len(nums) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return -1
+
 
