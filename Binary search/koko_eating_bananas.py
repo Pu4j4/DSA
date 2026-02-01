4e40acc0fb32f56dbe2945b1eec2781f8645e14c
pu4j4 <bhanubethala2@gmail.com>
2026-02-01 10:42:56 +0530

    #

--- Binary search/koko_eating_bananas.py
+++ Binary search/koko_eating_bananas.py
@@ -1,4 +1,5 @@
 #brute force
+import math
 def minEatingSpeed(self, piles, h):
     maxPile = max(piles)
     for k in range(1, maxPile + 1):
@@ -8,3 +9,24 @@ def minEatingSpeed(self, piles, h):
 
         if hours <= h:
             return k
+
+#optimized
+def minEatingSpeed(self, piles, h):
+    left, right = 1, max(piles)
+    ans = right
+
+    while left <= right:
+        mid = (left + right) // 2
+        hours = 0
+
+        for pile in piles:
+            hours += math.ceil(pile / mid)
+
+        if hours <= h:
+            ans = mid
+            right = mid - 1
+        else:
+            left = mid + 1
+
+    return ans
+
