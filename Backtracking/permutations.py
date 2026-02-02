385067a695da55bbb1b70817182876e1772b3802
pu4j4 <bhanubethala2@gmail.com>
2026-02-02 11:00:46 +0530

    #

--- Backtracking/permutations.py
+++ Backtracking/permutations.py
@@ -1,8 +1,32 @@
 #brute force
-from itertools import permutations
-
 def permute(self, nums):
     res = []
     for p in permutations(nums):
         res.append(list(p))
     return res
+
+
+#Optimized
+def permute(self, nums):
+    res = []
+    used = [False] * len(nums)
+
+    def backtrack(path):
+        if len(path) == len(nums):
+            res.append(path[:])
+            return
+
+        for i in range(len(nums)):
+            if used[i]:
+                continue
+
+            used[i] = True
+            path.append(nums[i])
+            backtrack(path)
+            path.pop()
+            used[i] = False
+
+    backtrack([])
+    return res
+from itertools import permutations
+
