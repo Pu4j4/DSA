59e2f8f82c3b1294882e3f69bcfa0fc9084306c7
pu4j4 <bhanubethala2@gmail.com>
2026-02-03 13:55:31 +0530

    #

--- Backtracking/combinations.py
+++ Backtracking/combinations.py
@@ -14,3 +14,24 @@ def combine(self, n, k):
 
     backtrack(1, [])
     return result
+
+
+#optimized
+def combine(self, n, k):
+    result = []
+
+    def backtrack(start, path):
+        if len(path) == k:
+            result.append(path[:])
+            return
+
+        # pruning: ensure enough numbers remain
+        for i in range(start, n - (k - len(path)) + 2):
+            path.append(i)
+            backtrack(i + 1, path)
+            path.pop()
+
+    backtrack(1, [])
+    return result
+
+
