188a8083c2928839e2a5359b21f69ffdda588981
pu4j4 <bhanubethala2@gmail.com>
2026-02-04 18:47:42 +0530

    #

--- Backtracking/permutations_ii.py
+++ Backtracking/permutations_ii.py
@@ -6,3 +6,33 @@ def permuteUnique(nums):
         res.add(p)
     return list(map(list, res))
 
+
+#Optimized
+def permuteUnique(self, nums):
+    nums.sort()
+    res = []
+    used = [False] * len(nums)
+
+    def backtrack(path):
+        if len(path) == len(nums):
+            res.append(path[:])
+            return
+
+        for i in range(len(nums)):
+            if used[i]:
+                continue
+
+            if i > 0 and nums[i] == nums[i - 1] and not used[i - 1]:
+                continue
+
+            used[i] = True
+            path.append(nums[i])
+            backtrack(path)
+            path.pop()
+            used[i] = False
+
+    backtrack([])
+    return res
+
+
+
