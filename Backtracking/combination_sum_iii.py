ff2b7a7be36734ec4c8cc3268ac46cb8ee620c3e
pu4j4 <bhanubethala2@gmail.com>
2026-02-07 11:53:41 +0530

    #

--- Backtracking/combination_sum_iii.py
+++ Backtracking/combination_sum_iii.py
@@ -21,3 +21,23 @@ def combinationSum3(self, k, n):
     generate(0, [])
     return res
 
+
+#optimized
+def combinationSum3(self, k, n):
+    res = []
+
+    def backtrack(start, remaining_sum, remaining_k, path):
+        if remaining_sum == 0 and remaining_k == 0:
+            res.append(path[:])
+            return
+
+        if remaining_sum < 0 or remaining_k < 0:
+            return
+
+        for i in range(start, 10):
+            path.append(i)
+            backtrack(i + 1, remaining_sum - i, remaining_k - 1, path)
+            path.pop()
+
+    backtrack(1, n, k, [])
+    return res
