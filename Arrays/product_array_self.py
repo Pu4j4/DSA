27e6d5b51eac77b7a5ede6ac4bb2e94a2f5a0d5e
pu4j4 <bhanubethala2@gmail.com>
2026-02-08 16:44:44 +0530

    #

--- Arrays/product_array_self.py
+++ Arrays/product_array_self.py
@@ -10,6 +10,23 @@ def  product_self(nums):
             product *= nums[j]
         res[i] = product
     return res
+
+
+#optimized
+def product_self(nums):
+    n = len(nums)
+    res = [1] * n
+    prefix = 1
+    for i in range(n):
+        res[i] = prefix
+        prefix *= nums[i]
+
+    suffix = 1
+    for i in range(n-1,-1,-1):
+        res[i] *= suffix
+        suffix *= nums[i]
+
+    return res
+
 nums = [1,2,4,6]
 print(product_self(nums))
-
