1bc2e0ff3be9578b9d2c7d70e272b9ec7b1bfb1a
pu4j4 <bhanubethala2@gmail.com>
2026-02-09 18:55:23 +0530

    #

--- Arrays/best_time_buy_and_sell.py
+++ Arrays/best_time_buy_and_sell.py
@@ -1,12 +1,22 @@
 #brute force
-def buy_sell(prices):
-    n = len(prices)
-    max_profit = 0
-    for i in range(n):
-        for j in range(i+1,n):
-            profit = prices[j] - prices[i]
-            max_profit = max(max_profit,profit)
-    return max_profit
+# def buy_sell(prices):
+#     n = len(prices)
+#     max_profit = 0
+#     for i in range(n):
+#         for j in range(i+1,n):
+#             profit = prices[j] - prices[i]
+#             max_profit = max(max_profit,profit)
+#     return max_profit
 
-prices = [8,3,1,2,5]
+
+#optimized
+def buy_sell(prices):
+    min_price = float('inf')
+    max_profit = 0
+    for price in prices:
+        min_price = min(price,min_price)
+        profit = price - min_price
+        max_profit = max(max_profit,profit)
+    return max_profit
+prices = [8,2,5,1,5,2]
 print(buy_sell(prices))
