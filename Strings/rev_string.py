c58adb0846ac1fa6d60b84e835b98af15fe0ec00
pu4j4 <bhanubethala2@gmail.com>
2026-02-11 10:01:29 +0530

    #

--- Strings/rev_string.py
+++ Strings/rev_string.py
@@ -12,3 +12,14 @@ print(rev_str(s))
 
 
 
+#optimized
+def rev_str(s):
+    n = len(s)
+    left, right = 0, n-1
+    while left<right:
+        s[left], s[right] = s[right], s[left]
+        left += 1
+        right -= 1
+    return s
+s = ["m","i","k","e"]
+print(rev_str(s))
