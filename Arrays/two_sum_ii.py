67241339a3a6b42e4750d8ad4a9fae28d0d55e13
pu4j4 <bhanubethala2@gmail.com>
2026-02-12 19:56:47 +0530

    #

--- Arrays/two_sum_ii.py
+++ Arrays/two_sum_ii.py
@@ -9,3 +9,17 @@ def two_sum_ii(numbers,target):
 numbers = [1,2,4,9]
 print(two_sum_ii(numbers, 6))
 
+#otimized
+def two_sum_ii(numbers, target):
+    left,right = 0, len(numbers)-1
+    while left<right:
+        curr_sum = numbers[left] + numbers[right]
+        if curr_sum == target:
+            return [left+1, right+1]
+        elif curr_sum < target:
+            left += 1
+        else:
+            right -= 1
+    return []
+numbers = [1,3,4,7,8]
+print(two_sum_ii(numbers, 7))
