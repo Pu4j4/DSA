d542d96324f83a9eb6f18e0712f7c9327a958ba8
pu4j4 <bhanubethala2@gmail.com>
2026-02-13 22:38:50 +0530

    #

--- Arrays/subarray_sum_k.py
+++ Arrays/subarray_sum_k.py
@@ -13,4 +13,18 @@ nums = [1,2,3]
 k = 3
 print(subarray_sum(nums,k))
 
+#optimized
+def subarray_sum(nums,k):
+    prefix_sum = 0
+    count = 0
+    prefix_map = {0:1}
+    for num in nums:
+        prefix_sum += num
+        if prefix_sum - k in prefix_map:
+            count += prefix_map[prefix_sum-k]
+        prefix_map[prefix_sum] = prefix_map.get(prefix_sum, 0) + 1
+    return count
 
+nums = [1,2,3,1,1,1]
+k = 3
+print(subarray_sum(nums,k))
