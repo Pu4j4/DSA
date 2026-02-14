20d6b49cdf7c71d3c095347517e7c82469c6f4f3
pu4j4 <bhanubethala2@gmail.com>
2026-02-14 14:28:19 +0530

    #

--- Stack/stack_using_queues.py
+++ Stack/stack_using_queues.py
@@ -25,3 +25,23 @@ class Stack:
     def empty(self):
         return len(self.q1) == 0
 
+
+
+#optimized
+class Stack:
+    def __init__(self):
+        self.q = deque()
+
+    def push(self,x):
+        self.q.append(x)
+        for _ in range(len(self.q)-1):
+            self.q.append(self.q.popleft())
+
+    def pop(self):
+        return self.q.popleft()
+
+    def top(self):
+        return self.q[0]
+
+    def empty(self):
+        return len(self.q) == 0
