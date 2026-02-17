ee1cdef870f5057ff0ebb76f1f833afe84adffa1
pu4j4 <bhanubethala2@gmail.com>
2026-02-17 18:10:55 +0530

    #

--- Stack/daily_temp.py
+++ Stack/daily_temp.py
@@ -10,3 +10,17 @@ def daily_temp(temps):
     return result
 temps = [54,57,80,35,48,90]
 print(daily_temp(temps))
+
+#optimized
+def daily_temp(temps):
+    n = len(temps)
+    result = [0]*n
+    stack = []
+    for i in range(n):
+        while stack and temps[i] > temps[stack[-1]]:
+            prev = stack.pop()
+            result[prev] = i - prev
+        stack.append(i)
+    return result
+temps = [20,48,30,40,68,29,39]
+print(daily_temp(temps))
