4e085e12a05d391141318aeb7e4cd6d502c93da3
pu4j4 <bhanubethala2@gmail.com>
2026-02-18 08:42:20 +0530

    #

--- Stack/evalRPN.py
+++ Stack/evalRPN.py
@@ -18,3 +18,25 @@ def evalrpn(tokens):
     return  int(tokens[0])
 tokens = ['5','7','*','2','+','4','-']
 print(evalrpn(tokens))
+
+
+#optimized
+def evalRPN(tokens):
+    stack = []
+    for token in tokens:
+        if token not in "+-*/":
+            stack.append(int(token))
+        else:
+            b = stack.pop()
+            a = stack.pop()
+            if token == '+':
+                stack.append(a+b)
+            elif token == '-':
+                stack.append(a-b)
+            elif token == '*':
+                stack.append(a*b)
+            else:
+                stack.append(int(a/b))
+    return stack[0]
+tokens = ['3','5','/','6','2','*','+']
+print(evalRPN(tokens))
