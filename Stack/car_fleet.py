6ca7ac435a2f6f012cf83fc237be5b1ec4272fcf
pu4j4 <bhanubethala2@gmail.com>
2026-02-18 08:43:16 +0530

    #

--- Stack/car_fleet.py
+++ Stack/car_fleet.py
@@ -15,6 +15,18 @@ def car_fleet(target,position,speed):
     return fleets
 
 
+#optimized
+def car_fleet(target,position,speed):
+    cars = list(zip(position, speed))
+    cars.sort(reverse=True)
+    stack = []
+    for pos, spd in cars:
+        time = (target-pos)/spd
+        stack.append(time)
+        if len(stack) >= 2 and stack[-1] <= stack[-2]:
+            stack.pop()
+    return len(stack)
+
 target = 12
 position = [10,8,0,5,3]
 speed    = [2,4,1,1,3]
