c6218e48d14d74c0f62d48453482855fe7be26e3
pu4j4 <bhanubethala2@gmail.com>
2026-02-26 10:10:50 +0530

    #

--- Sliding window/contains_duplicate_ii.py
+++ Sliding window/contains_duplicate_ii.py
@@ -9,7 +9,25 @@ def contains_dup(nums,k):
     return False
 
 
-
+#optimized
+def contains_dup(nums, k):
+    window = set()
+    for i in range(len(nums)):
+        if nums[i] in window:
+            return True
+        window.add(nums[i])
+        if len(window) >k:
+            window.remove(nums[i-k])
+    return False
+#or
+def contains_dup(nums,k):
+    n = len(nums)
+    index_map = {}
+    for i, num in enumerate(nums):
+        if num in index_map and i-index_map[num] <=k:
+            return True
+        index_map[num] = i
+    return False
 
 nums = [1,22,4,19,22,7]
 # nums = [1,2,3,4,2]
