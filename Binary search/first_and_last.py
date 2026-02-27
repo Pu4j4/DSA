e8d8f8767d24369dd7cfbcfb7cfba1dea1968232
pu4j4 <bhanubethala2@gmail.com>
2026-02-27 21:59:57 +0530

    #

--- Binary search/first_and_last.py
+++ Binary search/first_and_last.py
@@ -10,6 +10,37 @@ def search_range(nums,target):
     return [first, last]
 
 
+#optimized
+def search_range(nums,target):
+    def find_first():
+        left = 0
+        right = len(nums) - 1
+        ans = -1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                ans = mid
+                right = mid - 1
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return ans
 
+    def find_last():
+        left = 0
+        right = len(nums)-1
+        ans = -1
+        while left<=right:
+            mid = (left+right) // 2
+            if nums[mid] == target:
+                ans = mid
+                left = mid+1
+            elif nums[mid] < target:
+                left = mid+1
+            else:
+                right = mid-1
+        return ans
+    return [find_first(), find_last()]
 nums = [1,2,2,3,5,5]
 print(search_range(nums, 2))
