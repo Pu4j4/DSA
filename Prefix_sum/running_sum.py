7cefbae3bc816fed2084a73453cba1cd552b6fc5
pu4j4 <bhanubethala2@gmail.com>
2026-02-28 14:10:35 +0530

    #

--- Prefix_sum/running_sum.py
+++ Prefix_sum/running_sum.py
@@ -9,6 +9,22 @@ def running_sum(nums):
     return result
 
 
+
+#optimized - i
+def running_sum(nums):
+    result = []
+    total = 0
+    for num in nums:
+        total += num
+        result.append(total)
+    return result
+
+#optimized - ii -modifying same array
+def running_sum(nums):
+    for i in range(1, len(nums)):
+        nums[i] = nums[i-1] + nums[i]
+    return nums
+
 nums = [1,13,17,20]
 print(running_sum(nums))
 
