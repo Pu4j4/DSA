7a7be23cb4402b10fc927b6ff9e8298f284d63f8
pu4j4 <bhanubethala2@gmail.com>
2026-03-04 08:40:30 +0530

    #

--- HashMap/long_consecutive_sequence.py
+++ HashMap/long_consecutive_sequence.py
@@ -17,7 +17,17 @@ def longestConsecutive(nums) :
 
 
 
-
+# optimized
+def longestConsecutive( nums):
+    max_len = 0
+    num_set = set(nums)
+    for num in num_set:
+        if num - 1 not in num_set:
+            curr_len = 1
+            while num + curr_len in num_set:
+                curr_len += 1
+            max_len = max(max_len, curr_len)
+    return max_len
 
 
 
