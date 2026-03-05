a2cfb0bb4dee72ffee821dfaa66dee54f7cf8c32
pu4j4 <bhanubethala2@gmail.com>
2026-03-05 08:04:22 +0530

    #

--- Arrays/contains_duplicate.py
+++ Arrays/contains_duplicate.py
@@ -1,3 +1,15 @@
+# Problem: Contains Duplicate (https://leetcode.com/problems/contains-duplicate/)
+
+# Problem statement:
+# Given an integer array nums return True iif any value appears atleast twice in the array,
+# and return False if every element is distinct
+
+#pattern: Hashset
+
+#Brute force idea:
+# for every element compare with another element using nested loops
+# if it contains duplicate return true else False
+
 #brute force
 # def contain_duplicate(nums):
 #     n = len(nums)
@@ -9,6 +21,17 @@
 #
 # print(contain_duplicate([1,3,4,2,1,2]))
 
+# Time: O(n^2) - nested loops   space: O(1)
+
+# Why it's slow:
+#Checking every possible pair using nested loops
+#repeated comparisons
+
+# optimized idea
+#instead of checking every element, we store elements in hashset
+#check if element in hashset, return True if not add current element to set
+
+
 #optimized (using hashset())
 def contains_duplicate(nums):
     seen = set()
@@ -19,3 +42,5 @@ def contains_duplicate(nums):
     return False
 
 print(contains_duplicate([1,3,4,2]))
+
+#Time: O(n)- single loop    Space: O(n)-hashset stores n elements
