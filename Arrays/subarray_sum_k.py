33651b5e5b320b4740b9aae794db2d161f3d9086
pu4j4 <bhanubethala2@gmail.com>
2026-03-06 09:35:17 +0530

    #

--- Arrays/subarray_sum_k.py
+++ Arrays/subarray_sum_k.py
@@ -1,3 +1,15 @@
+# Problem: Subarray sum equals k (https://leetcode.com/problems/subarray-sum-equals-k/description/)
+
+# Problem statement:
+# Given an array nums and integer k, return the total no.of subarrays whose sum equals k
+
+# Pattern: Prefix_sum + hashmap
+
+#brute force idea
+#check every possible subarray and calculate its sum
+#start from each index i,extend the subarray with index j one by one
+#keep each num adding to sum, if sum equals k -> increase count -> return count
+
 #brute force
 def subarray_sum(nums,k):
     n = len(nums)
@@ -12,6 +24,18 @@ def subarray_sum(nums,k):
 nums = [1,2,3]
 k = 3
 print(subarray_sum(nums,k))
+
+#Time: O(n^2)- nested loops     space: O(1)
+
+#why it's slow
+#checking all subarrays, slow for large input
+
+#optimized idea
+#instead of recalculating sum every time, reuse previous sum
+#prefix_sum[j] - prefix_sum[i] = k --> rearrange - prefix_sum[i] = prefix_sum[j]-k
+#so check prefix_sum-k exists before
+#use hashmap to store prefix sums
+
 
 #optimized
 def subarray_sum(nums,k):
@@ -28,3 +52,6 @@ def subarray_sum(nums,k):
 nums = [1,2,3,1,1,1]
 k = 3
 print(subarray_sum(nums,k))
+
+
+#Time: O(n) -single loop,O(1) lookup   space-O(n) - stores prefix sums
