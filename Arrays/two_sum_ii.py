04bb33c094bb62ea9efeb3a67e1a206f6c05091b
pu4j4 <bhanubethala2@gmail.com>
2026-03-06 09:34:24 +0530

    #

--- Arrays/two_sum_ii.py
+++ Arrays/two_sum_ii.py
@@ -1,3 +1,15 @@
+# Problem:  Two Sum II - Input Array Is Sorted (https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/)
+
+# Problem statement:
+# Given a sorted array of integer nums (sorted in non_decreasing order) and an integer target.
+# find two numbers such that they add up to target, return 1-based indices of two numbers
+
+#pattern : Two pointers
+
+#brute force idea
+# loop through the nums and calculate sum
+# check if sum equal to target, if yes return indices
+
 #brute force
 def two_sum_ii(numbers,target):
     n = len(numbers)
@@ -8,6 +20,17 @@ def two_sum_ii(numbers,target):
 
 numbers = [1,2,4,9]
 print(two_sum_ii(numbers, 6))
+
+#Time: O(n^2) - nested loops  Space: O(1)
+
+#why it's slow
+# checks all pairs, repeated comparisons
+
+#optimized
+#Use two pointers - left at start, right at end
+#calculate sum and check if sum equal to target return indices
+#if sum too small, left ++
+#if sum to large, right--
 
 #otimized
 def two_sum_ii(numbers, target):
@@ -23,3 +46,5 @@ def two_sum_ii(numbers, target):
     return []
 numbers = [1,3,4,7,8]
 print(two_sum_ii(numbers, 7))
+
+#Time: O(n) - each element visited once by pointers   Space: O(1) - only Two variables used
