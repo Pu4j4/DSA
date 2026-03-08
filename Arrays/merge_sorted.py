97b3295ba27576190d6523ddcbdc5c95fbfc0786
pu4j4 <bhanubethala2@gmail.com>
2026-03-08 15:23:49 +0530

    #

--- Arrays/merge_sorted.py
+++ Arrays/merge_sorted.py
@@ -1,10 +1,33 @@
-#brute force
-# def merge_sorted(nums1,m,nums2,n):
-#     for i in range(n):
-#         nums1[m+i] = nums2[i]
-#     nums1.sort()
-#     return nums1
+# problem: Merge sorted array (https://leetcode.com/problems/merge-sorted-array/description/)
 
+# Problem statement:
+# Given two sorted arrays nums1 and nums2, integers m(represents actual elements in nums1) and n(elements in nums2)
+# nums1 has size m+n
+# first m elements are valid,last n elements are 0
+#merge nums2 into nums1 such that nums1 becomes one sorted array - in-place
+
+#Pattern - Two pointers(three pointer(reverse direction) - extra pointer to place position)
+
+#brute force idea
+#traverse nums1 and add elements from m+i of nums2
+#sort the nums1
+
+#brute force
+def merge_sorted(nums1,m,nums2,n):
+    for i in range(n):
+        nums1[m+i] = nums2[i]
+    nums1.sort()
+    return nums1
+
+#Time: O((m+n) log (m+n)) - sorting n=(m+n)-n log n    Space: O(1)
+
+#why it's slow
+#sorting takes - (m+n) log (m+n) - optimize to O(m+n)
+
+#optimized idea
+#compare largest elements from both arrays
+#place then at the end of array
+#move backwards
 
 #otimized
 def merge_sorted(nums1,m,nums2,n):
@@ -26,3 +49,5 @@ nums2 = [2,5,9]
 m = 3
 n = 3
 print(merge_sorted(nums1,m,nums2,n))
+
+#Time: O(m+n)- each element processed once     Space: O(1) - in-place
