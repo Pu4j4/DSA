1bf1c52de47f2c39f3e09bc05f1dba66b93f3004
pu4j4 <bhanubethala2@gmail.com>
2026-03-18 13:55:04 +0530

    #

--- Binary search/binary_search.py
+++ Binary search/binary_search.py
@@ -1,12 +1,39 @@
-#brute force
-def search(self, nums, target):
+#Problem : Binary search (https://leetcode.com/problems/binary-search/)
+
+#Problem statement:
+#Given a sorted array integer nums in ascending order and a target value
+#return the indec of target if it exists in the array , otherwise return -1
+
+#Pattern: Binary search
+
+#brute force idea
+#loop through each element index i from o to n-1
+#if target element found in array return its index else return -1
+
+#brute force code
+def search(nums, target):
     for i in range(len(nums)):
         if nums[i] == target:
             return i
     return -1
 
-#optimized
-def search(self, nums: List[int], target: int) -> int:
+#Time: O(n) - check every element one by one   Space: O(1) - no extra space
+
+#why it's slow:
+#checks every element, n comparisons
+#need O(log n) complexity
+
+#Optimized idea
+#array is sorted, we can eliminate half elements each time:
+#use left and right pointers, left = 0 right = len(nums)-1
+#take middle element: compare with target
+#if equal -> return middle element index
+#if small -> left = mid+1
+#if large -> right = mid-1
+#otherwise return -1
+
+#optimized code
+def search(nums, target):
     left, right = 0, len(nums) - 1
 
     while left <= right:
@@ -21,4 +48,7 @@ def search(self, nums: List[int], target: int) -> int:
 
     return -1
 
+nums = [1,3,5,6,8,9]
+print(search(nums,6))
 
+#Time: O(log n)  - each halves search space     Space: O(1) - only variables used
