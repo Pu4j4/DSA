73ca1916b39ba309c06d6cf51c9a5edaffba1e6b
pu4j4 <bhanubethala2@gmail.com>
2026-03-23 20:37:56 +0530

    #

--- Binary search/search_rotated_array.py
+++ Binary search/search_rotated_array.py
@@ -1,11 +1,34 @@
-#brute force
+#Problem: Search in rotated sorted array (https://leetcode.com/problems/search-in-rotated-sorted-array/description/)
+
+#Problem statement:
+#Given an integer sorted array that has been rotated at unknown pivot(position) and integer target
+#return the index of target if found, otherwise return -1
+
+#Pattern: Binary search
+
+#brute force idea
+#loop through each and every element
+#if target found return index if not return -1
+
+#brute force code
 def search(nums, target):
     for i in range(len(nums)):
         if nums[i] == target:
             return i
     return -1
 
-#optimized
+#Time: O(n)- checks each element     Space:O(1) - no space used
+
+#why it's slow
+#checks all elements
+#slow for large input, need O(log n) complexity
+
+#optimized idea
+#even though is rotated, one half is always sorted
+#checks:  if left half sorted ->check if target in left half
+#else: search right half
+
+#optimized code
 def search(nums, target):
     left, right = 0, len(nums) - 1
 
@@ -34,3 +57,5 @@ def search(nums, target):
 
 nums = [4,5,6,1,2,3]
 print(search(nums, 2))
+
+#Time: O(log n) - search space halves each iteration    Space: O(1) - only pointers used
