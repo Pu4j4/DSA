45f35998d67f597d7ae6ad4f243ac7ba92f6e795
pu4j4 <bhanubethala2@gmail.com>
2026-04-30 22:25:09 +0530

    #

--- Dynamic_programming/max_product_subarray.py
+++ Dynamic_programming/max_product_subarray.py
@@ -14,6 +14,23 @@ def max_product(nums):
 
     return max_product
 
-nums = [2,3,-2,4]
-print(max_product(nums))
 
+
+def max_product(nums) -> int:
+    curr_max = nums[0]
+    curr_min = nums[0]
+    result = nums[0]
+
+    for i in range(1, len(nums)):
+
+        num = nums[i]
+
+        if num < 0:
+            curr_max, curr_min = curr_min, curr_max
+
+        curr_max = max(num, curr_max * num)
+        curr_min = min(num, curr_min * num)
+
+        result = max(result, curr_max)
+
+    return result
